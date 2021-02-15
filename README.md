40f79b3ca0e0f002ca9b582cd1bb38bb550bbd6b
Rafael Cid <rcid@optaresolutions.com>
2021-02-15 12:21:17 +0100

    Add new business rules

---
+++ README.md
@@ -0,0 +1,11 @@
+# VACCINES BACKEND
+
+## Features
+* Retrieve all persons
+* Retrieve all vaccines
+* Vaccinate the following people according to the next priority :
+    * Vaccine persons with age > 65
+    * Vaccine Mayors
+    * Vaccine Health personnel
+    * The rest of the population
+
--- spaghetti/src/main/java/com/optare/interview/MainApplication.java
+++ spaghetti/src/main/java/com/optare/interview/MainApplication.java
@@ -20,6 +20,25 @@ public class MainApplication {
         SpringApplication.run(MainApplication.class, args);
     }
 
+    @GetMapping("/vaccines")
+    public List<VaccineResponse> listOVaccined() {
+
+        List<VaccineResponse> vaccineResponses = new ArrayList<>();
+
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://localhost:1080/vaccines";
+        ResponseEntity<String> responseVaccines = restTemplate.getForEntity(url, String.class);
+        JSONArray var0 = JsonPath.read(responseVaccines.getBody(), "$");
+        for (Object x : var0) {
+            String var1 = JsonPath.read(x, "$.id");
+            String var2 = JsonPath.read(x, "$.type");
+            vaccineResponses.add(new VaccineResponse(var1, var2));
+        }
+
+        return vaccineResponses;
+
+    }
+
     @GetMapping("/persons")
     public List<PersonResponse> listOfPersonsVaccined(@RequestParam(required = false) String vaccineType) {
 
@@ -35,7 +54,8 @@ public class MainApplication {
                 String var1 = JsonPath.read(x, "$.id");
                 String var2 = JsonPath.read(x, "$.name");
                 String var3 = JsonPath.read(x, "$.age");
-                result.add(new PersonResponse(var1, var2, Integer.parseInt(var3)));
+                String var6 = JsonPath.read(x, "$.job");
+                result.add(new PersonResponse(var1, var2, Integer.parseInt(var3), var6));
             }
 
             return result;
@@ -51,7 +71,8 @@ public class MainApplication {
                 String var1 = JsonPath.read(x, "$.id");
                 String var2 = JsonPath.read(x, "$.name");
                 String var3 = JsonPath.read(x, "$.age");
-                persons.add(new PersonResponse(var1, var2, Integer.parseInt(var3)));
+                String var6 = JsonPath.read(x, "$.job");
+                persons.add(new PersonResponse(var1, var2, Integer.parseInt(var3), var6));
             }
 
             List<PersonResponse> personsVaccinated = new ArrayList<>();
@@ -90,27 +111,62 @@ public class MainApplication {
             String var1 = JsonPath.read(x, "$.id");
             String var2 = JsonPath.read(x, "$.name");
             String var3 = JsonPath.read(x, "$.age");
-            persons.add(new PersonResponse(var1, var2, Integer.parseInt(var3)));
+            String var6 = JsonPath.read(x, "$.job");
+            persons.add(new PersonResponse(var1, var2, Integer.parseInt(var3), var6));
         }
 
-        String url = "http://localhost:1080/vaccines?vaccineType=" + vaccineRequest.getVaccineType();
+        String url = "http://localhost:1080/vaccines?notVaccinated=true&vaccineType=" + vaccineRequest.getVaccineType();
         ResponseEntity<String> responseVaccines = restTemplate.getForEntity(url, String.class);
         JSONArray var0 = JsonPath.read(responseVaccines.getBody(), "$");
-      List<String> personsVaccinated = new ArrayList<>();
-        for (Object x : var0) {
-            String var1 = JsonPath.read(x, "$.id");
-            String var2 = JsonPath.read(x, "$.type");
-            String var3 = JsonPath.read(x, "$.personId");
+        int totalVaccines = var0.size();
 
-            // Business logic
-            if (StringUtils.isEmpty(var3)) {
-                for (PersonResponse person : persons) {
-                    if (person.getAge() > 30 && !personsVaccinated.contains(person.getId())) {
-                        String url3 = "http://localhost:1080/vaccines/"+var1;
-                        PersonRequest personRequest = new PersonRequest(person.getId());
-                        restTemplate.put(url3, personRequest);
-                        personsVaccinated.add(person.id);
-                        System.out.println(person.getName() + " was vaccinated (" + var2+ ")");
+        List<String> personsVaccinated = new ArrayList<>();
+        while (totalVaccines > 0 ) {
+            for (Object x : var0) {
+                String var1 = JsonPath.read(x, "$.id");
+                String var2 = JsonPath.read(x, "$.type");
+                String var3 = JsonPath.read(x, "$.personId");
+
+                // Business logic
+                if (StringUtils.isEmpty(var3)) {
+
+                    for (PersonResponse person : persons) {
+                        if (person.getAge() > 65 && !personsVaccinated.contains(person.getId())) {
+                            String url3 = "http://localhost:1080/vaccines/" + var1;
+                            PersonRequest personRequest = new PersonRequest(person.getId());
+                            restTemplate.put(url3, personRequest);
+                            personsVaccinated.add(person.id);
+                            totalVaccines--;
+                            person.setVaccinated(true);
+                            System.out.println("Rule (age > 65): " + person.getName() + " was vaccinated (" + var2 + ")");
+                        }
+                        if (notHasRetiredPersons(persons) && person.getJob().equals("Mayor") && !personsVaccinated.contains(person.getId())) {
+                            String url3 = "http://localhost:1080/vaccines/" + var1;
+                            PersonRequest personRequest = new PersonRequest(person.getId());
+                            restTemplate.put(url3, personRequest);
+                            personsVaccinated.add(person.id);
+                            totalVaccines--;
+                            person.setVaccinated(true);
+                            System.out.println("Rule (Mayor): " + person.getName() + " was vaccinated (" + var2 + ")");
+                        }
+                        if (notHasRetiredPersons(persons) && notHasMayors(persons) && person.getJob().equals("HealthPersonnel") && !personsVaccinated.contains(person.getId())) {
+                            String url3 = "http://localhost:1080/vaccines/" + var1;
+                            PersonRequest personRequest = new PersonRequest(person.getId());
+                            restTemplate.put(url3, personRequest);
+                            personsVaccinated.add(person.id);
+                            totalVaccines--;
+                            person.setVaccinated(true);
+                            System.out.println("Rule (Health Personnel): " + person.getName() + " was vaccinated (" + var2 + ")");
+                        }
+                        if (notHasRetiredPersons(persons) && !hasHealthPersonnel(persons) && notHasMayors(persons) && !personsVaccinated.contains(person.getId())) {
+                            String url3 = "http://localhost:1080/vaccines/" + var1;
+                            PersonRequest personRequest = new PersonRequest(person.getId());
+                            restTemplate.put(url3, personRequest);
+                            personsVaccinated.add(person.id);
+                            totalVaccines--;
+                            person.setVaccinated(true);
+                            System.out.println("Rule (The rest of population): " + person.getName() + " was vaccinated (" + var2 + ")");
+                        }
                     }
                 }
             }
@@ -120,16 +176,48 @@ public class MainApplication {
 
     }
 
-    public class PersonResponse {
+    private boolean notHasMayors(List<PersonResponse> persons) {
+        for (PersonResponse p : persons) {
+            if (!p.isVaccinated() && p.getJob().equals("Mayor")) return false;
+        }
+        return true;
+    }
+
+    private boolean hasHealthPersonnel(List<PersonResponse> persons) {
+        for (PersonResponse p : persons) {
+            if (!p.isVaccinated() && p.getJob().equals("HealthPersonnel")) return true;
+        }
+        return false;
+    }
+
+    private boolean notHasRetiredPersons(List<PersonResponse> persons) {
+        for (PersonResponse p : persons) {
+            if (!p.isVaccinated() && p.getAge() > 65) return false;
+        }
+        return true;
+    }
+
+    public static class PersonResponse {
 
         private String id;
         private String name;
         private Integer age;
+        private String job;
+        private boolean vaccinated;
 
-        public PersonResponse(String id, String name, Integer age) {
+        public PersonResponse(String id, String name, Integer age, String job) {
             this.id = id;
             this.name = name;
             this.age = age;
+            this.job = job;
+        }
+
+        public String getJob() {
+            return job;
+        }
+
+        public void setJob(String job) {
+            this.job = job;
         }
 
         public String getId() {
@@ -154,6 +242,14 @@ public class MainApplication {
 
         public void setAge(Integer age) {
             this.age = age;
+        }
+
+        public void setVaccinated(boolean vaccinated) {
+            this.vaccinated = vaccinated;
+        }
+
+        protected boolean isVaccinated() {
+            return vaccinated;
         }
     }
 
